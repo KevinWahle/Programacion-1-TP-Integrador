12b20c465e9960d74bcea1e07d6ecb1832a8c28d
Francisco Basili <57078209+FranBasili@users.noreply.github.com>
2020-07-23 20:08:07 -0300

    .

---
+++ Backend/events.c
@@ -0,0 +1,25 @@
+/*
+    * Mov up
+    * Mov down
+    * Mov left
+    * Mov right
+    * Click
+    * Pause/exit
+*/
+
+enum CONTROLS {
+    MOVE_UP,
+    MOVE_DOWN,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    CLICK_BTN,  // Botón de click, shoot o aceptar
+    PAUSE_BTN
+};
+
+
+int get_device_status()
+{
+
+}
+
+
