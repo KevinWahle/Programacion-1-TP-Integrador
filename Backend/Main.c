cb516ceff48adeddef518b012348f339e93c4411
Francisco Basili <57078209+FranBasili@users.noreply.github.com>
2020-07-23 16:40:20 -0300

    Dia 1

--- Backend/Main.c
+++ Backend/Main.c
@@ -1,13 +1,20 @@
 
 typedef unsigned char BYTE;
-typedef struct state_diagram_edge STATE;
+//typedef struct state_diagram_edge STATE;
 
-struct state_diagram_edge{
+typedef struct state_diagram_edge
+{
     BYTE evento;
     STATE *proximo_estado;
     void (*p_rut_accion)(void);
-};
+}STATE;
 
+STATE estado_pito[] = {         //TODO: considerar otro nombre del estado
+    {Evento1, estado_1, rut_acc1},
+    {Evento1, estado_1, rut_acc1}, 
+    {Evento1, estado_1, rut_acc1},
+    {FIN_TABLE, estado_0, rut_acc1}
+};
 
 
 int main()
@@ -15,4 +22,16 @@ int main()
     
     
     return 0;
+}
+
+void fsm()
+{
+    printf(" >>%c<<\n ",evento_actual); // Para debuggear
+
+    while (p_tabla_estado -> evento != evento_actual && p_tabla_estado -> evento !=FIN_TABLA)
+    ++p_tabla_estado;
+    
+    (*p_tabla_estado -> p_rut_accion) (); // 2- Ejecuta Rutina de accion corresondiente
+    p_tabla_estado=p_tabla_estado -> proximo_estado; // 3- Encuentro próximo estado
+    return(p_tabla_estado);    
 }
--- compile.bash
+++
@@ -1,2 +0,0 @@
-#Uso: compile "source" "target". Ej: compile main.c main
-gcc $0 -Wall -o $1 -l allegro -l allegro_audio -l allegro_acodec -l allegro_color -l allegro_font -l allegro_image -l allegro_primitives
