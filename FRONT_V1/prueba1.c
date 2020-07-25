d47422c09ab07ffea1d1a9718854da866d435552
bautistasch <bschneeberger@itba.edu.ar>
2020-07-24 23:14:54 -0300

    pushh

--- FRONT_V1/prueba1.c
+++ FRONT_V1/prueba1.c
@@ -1,5 +1,3 @@
-
-
 /*******************************************************************************
  * INCLUDE HEADER FILES
  ******************************************************************************/
@@ -15,7 +13,7 @@
 #define D_WIDTH 640
 #define D_HEIGHT 480
 #define TASA_DE_CAMBIO 3
-#define TASA_DE_CAMBIO_BALA 5
+#define TASA_DE_CAMBIO_BALA 2
 
 #define SHOT_HEIGHT 15
 #define SHOT_WIDTH 4
@@ -23,28 +21,40 @@
 #define COLA_DE_DISPARO (450 - SHOT_HEIGHT)
 #define POS_EN_Y_DEL_CANNON 450
 
-#define CANT_INVADERS 10
-#define HEIGHT_INAVDERS_SEPARATION 15
+#define FIL_INVADERS 5
+#define COL_INVADERS 9
 
+
+#define CANT_INVADERS FIL_INVADERS*COL_INVADERS
+
+#define HIGH_INVADERS_SEPARATION 15
 
 #define CANT_D_POSIBLES_OBJETIVOS CANT_INVADERS // Solo tengo en cuenta disparo del cannon hacia los invaders no al reves.
 
 #define CANT_D_ESCUDOS_POSIBLES 5 // falta para usar esto. No serian 5 habrian como 6 bloquecitos por cada bloque.
 
-int initAll(void);
+#define MAX_INVADERS_SHOT 20
 
-int isCollision(int x1,int y1,int h1,int w1,int x2,int y2,int h2,int w2);
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+enum SHOT_TYPES {CANON_SHOT, INVADER_SHOT};
 
 enum MYKEYS {
     KEY_SPACE, KEY_DOWN, KEY_LEFT, KEY_RIGHT //arrow keys
 };
+
+typedef unsigned char SHOT_TYPE;
 
 typedef struct 
 {
     int x;
     int y;
     int shotState;
-}cannonShot_t;
+    SHOT_TYPE type;
+}shot_t;
+
 
 typedef struct 
 {
@@ -59,21 +69,57 @@ typedef struct
     int x;
     int y;
     int invaderState;
+    int invaderType;
+    ALLEGRO_BITMAP *invadersPointer;
 }invader_t;
+
 
 typedef int cannonPosition_t;
 
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
 
-void placeInvaders(ALLEGRO_BITMAP **ptr_to_invaders, invader_t *ptr_to_struct);
-void drawAliveInvaders(ALLEGRO_BITMAP **ptr_to_invaders, invader_t *ptr_to_struct);
+int initAll(void);
+
+int isCollision( collBoxShot_t * box1, collBoxShot_t * box2);
+
+int invaderShot(int i, int j);
+
+void getInvaderShotCollison(void);
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+static void placeInvaders(invader_t ptr_to_struct[FIL_INVADERS][COL_INVADERS]);
+static void drawAliveInvaders(invader_t ptr_to_struct[FIL_INVADERS][COL_INVADERS]);
+
+
+/*******************************************************************************
+ * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+static shot_t invaderShotList[MAX_INVADERS_SHOT];
+// Lista de los disparos de los invaders
+
+static int actualShots; 
+
+/************ALLEGRO VARIABLES DEFINITION***************/
+static ALLEGRO_DISPLAY * display = NULL;
+static ALLEGRO_BITMAP *image = NULL;
+static ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+static ALLEGRO_TIMER *timer = NULL;
+
+// El cañón
+cannonPosition_t cannonXpos = 0;
+
+// Invaders matrix
+static invader_t invaders[FIL_INVADERS][COL_INVADERS];
+
 
 int main(void) {
-    ALLEGRO_DISPLAY * display = NULL;
-    ALLEGRO_BITMAP *image = NULL;
-    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-    ALLEGRO_TIMER *timer = NULL; 
 
-    ALLEGRO_BITMAP *invadersPointer[CANT_INVADERS];
+    srand (time(NULL));
 
     bool redraw = false;
     bool display_close = false;
@@ -81,71 +127,11 @@ int main(void) {
     bool key_pressed[4] = {false, false, false, false}; //Estado de teclas, true cuando esta apretada
 
     if(initAll())
-        return -1;
-
-    timer = al_create_timer(1.0 / FPS); //crea el timer pero NO empieza a correr
-    if (!timer) {
-        fprintf(stderr, "failed to create timer!\n");
-        return -1;
-    }
-
-    event_queue = al_create_event_queue();
-    if (!event_queue) {
-        fprintf(stderr, "failed to create event_queue!\n");
-        al_destroy_timer(timer);
-        return -1;
-    }
-
-    image = al_load_bitmap("Laser_Cannon.png");
-    if (!image) {
-        fprintf(stderr, "failed to load image !\n");
-        return 0;
-    }
-
-
-    for (int i = 0; i < CANT_INVADERS; i++)                         //Cargo el bitmap a cada puntero
     {
-        invadersPointer[i] = al_load_bitmap("Laser_Cannon.png");
-        if (!image) {
-            fprintf(stderr, "failed to load image !\n");
-            return 0;
-        }
+        return -1;
     }
     
-
-
-    display = al_create_display(D_WIDTH, D_HEIGHT); // Intenta crear display de 640x480 de fallar devuelve NULL
-    if (!display) {
-        fprintf(stderr, "failed to create display!\n");
-        return -1;
-    }
-
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-    al_register_event_source(event_queue, al_get_timer_event_source(timer));
-    al_register_event_source(event_queue, al_get_keyboard_event_source()); //REGISTRAMOS EL TECLADO
-
-
-
-    invader_t invaders[CANT_INVADERS];
-    placeInvaders(invadersPointer, invaders );
-
-    ALLEGRO_BITMAP* cuadrado = al_load_bitmap("Laser_Cannon.png");
-    al_draw_bitmap(cuadrado, 100, 100, 0);
-
-    //void al_draw_bitmap(ALLEGRO_BITMAP *bitmap, float dx, float dy, int flags) 
-    al_draw_bitmap(image, 0, 450, 0); //flags(normalmente en cero, ver doc. para rotar etc)
-
-    al_flip_display(); //Flip del backbuffer, pasa a verse a la pantalla
-
-
-    al_start_timer(timer); //Recien aca EMPIEZA el timer
-
-
-    //al_rest(5.0); ######################################################
-
-
-    cannonPosition_t cannonXpos = 0;
-    cannonShot_t shot1 = {0,0,0};
+    shot_t shot1 = {0,0,0};
     collBoxShot_t collBoxShot = {0,0,0,0};
 
     while (!display_close) {
@@ -197,12 +183,17 @@ int main(void) {
                     break;
                 }
             }
-            
         }
         if (redraw && al_is_event_queue_empty(event_queue)) {
             redraw = false;
 
             al_clear_to_color(al_map_rgb(0, 0, 0));
+
+            if(  !(rand() % 30)  )
+            {
+                invaderShot(FIL_INVADERS - 1 , rand()  % (COL_INVADERS )  );
+            }
+
             if(key_pressed[KEY_SPACE] && !shot1.shotState) 
             {
                 int ship_width = al_get_bitmap_width(image);
@@ -214,8 +205,9 @@ int main(void) {
                 shot1.x = (ship_width + 2*cannonXpos)/2;
                 shot1.y = 450;
                 collBoxShot.x = shot1.x - SHOT_WIDTH/2;
-                //collBoxShot.x = cannonXpos; ESTO ES RARO
                 collBoxShot.y = shot1.y - SHOT_HEIGHT;
+                collBoxShot.width = SHOT_WIDTH;
+                collBoxShot.height = SHOT_HEIGHT;
             }
             if(shot1.shotState)
             {
@@ -223,25 +215,37 @@ int main(void) {
                 shot1.y -= TASA_DE_CAMBIO_BALA;
                 collBoxShot.y = shot1.y - SHOT_HEIGHT;
 
-                for (int i = 0; i < CANT_D_POSIBLES_OBJETIVOS; i++)
+                for (int i = 0; i < FIL_INVADERS; i++)
                 {
-                    if( (invaders[i]).invaderState  )
+                    for (int j = 0; j < COL_INVADERS; j++)
                     {
-                        if( isCollision((invaders[i]).x, (invaders[i]).y,al_get_bitmap_height(invadersPointer[i]), al_get_bitmap_width(invadersPointer[i]), collBoxShot.x, collBoxShot.y, SHOT_HEIGHT, SHOT_WIDTH) )
+                        if( invaders[i][j].invaderState  )
                         {
-                            printf("COLISION!!!!!\n");
-                            shot1.shotState = 0;
-                            (invaders[i]).invaderState = 0;
+                            collBoxShot_t invaderBox = {.x = invaders[i][j].x,
+                                                        .y = invaders[i][j].y,
+                                                        .height = al_get_bitmap_height(invaders[i][j].invadersPointer),
+                                                        .width = al_get_bitmap_width(invaders[i][j].invadersPointer)};
+                            if( isCollision( &invaderBox, &collBoxShot))
+                            {
+                                printf("COLISION!!!!!\n");
+                                shot1.shotState = 0;
+                                invaders[i][j].invaderState = 0;
+                            }
                         }
                     }
                 }
+
+                //TODO: Chequear colision con escudos
+
                 if( collBoxShot.y <= 0)
                 {
                     shot1.shotState = 0;
                 }
             }
-            //al_draw_bitmap(cuadrado, 100, 100, 0);
-            drawAliveInvaders(invadersPointer, invaders);
+
+            getInvaderShotCollison();
+            
+            drawAliveInvaders(invaders);
             al_draw_bitmap(image, cannonXpos, 450, 0); //flags(normalmente en cero, ver doc. para rotar etc)
             al_flip_display(); 
         }
@@ -252,27 +256,35 @@ int main(void) {
     al_destroy_bitmap(image);
     al_destroy_event_queue(event_queue);
     al_destroy_timer(timer);
-    al_destroy_bitmap(cuadrado);
-    //al_init es "destruido automaticamente" ver documentacion 
+  
+      //al_init es "destruido automaticamente" ver documentacion 
 
-    for (int i = 0; i < CANT_INVADERS; i++)
-    {
-        al_destroy_bitmap(invadersPointer[i]);
+    for (int i = 0; i < FIL_INVADERS; i++) {
+        for (int j = 0; j < COL_INVADERS; j++) {
+            al_destroy_bitmap(invaders[i][j].invadersPointer);
+        }
     }
 
     return 0;
 }
 
-int isCollision(int x1,int y1,int h1,int w1,int x2,int y2,int h2,int w2){
+int isCollision(collBoxShot_t* box1, collBoxShot_t* box2){
 
-    if(x1 < x2 + w2 &&
-       x2 < x1 + w1 &&
-       y1 < y2 + h2 &&
-       y2 < y1 + h1)
+    if(box1->x < box2->x + box2->width &&
+       box2->x < box1->x + box1->width &&
+       box1->y < box2->y + box2->height &&
+       box2->y < box1->y + box1->height)
        return 1;
 
     return 0;
 }
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
 int initAll(void)
 {
     if (!al_init()) { //Primera funcion a llamar antes de empezar a usar allegro.
@@ -295,35 +307,176 @@ int initAll(void)
         fprintf(stderr, "failed to initialize image addon !\n");
         return -1;
     }
+
+
+    timer = al_create_timer(1.0 / FPS); //crea el timer pero NO empieza a correr
+    if (!timer) {
+        fprintf(stderr, "failed to create timer!\n");
+        return -1;
+    }
+
+    event_queue = al_create_event_queue();
+    if (!event_queue) {
+        fprintf(stderr, "failed to create event_queue!\n");
+        al_destroy_timer(timer);
+        return -1;
+    }
+
+    image = al_load_bitmap("Laser_Cannon.png");
+    if (!image) {
+        fprintf(stderr, "failed to load image !\n");
+        return -1;
+    }
+
+    for (int i = 0; i < FIL_INVADERS; i++)
+    {
+        for (int j = 0; j < COL_INVADERS; j++)                         //Cargo el bitmap a todas las invaders
+        {
+            invaders[i][j].invadersPointer = al_load_bitmap("Laser_Cannon.png");
+            if (!invaders[i][j].invadersPointer) {
+                fprintf(stderr, "failed to load image !\n");
+                return -1;
+            }
+
+            //TODO: Switch case segun el tipo de invader
+        }
+    }
+
+    display = al_create_display(D_WIDTH, D_HEIGHT); // Intenta crear display de 640x480 de fallar devuelve NULL
+    if (!display) {
+        fprintf(stderr, "failed to create display!\n");
+        return -1;
+    }
+
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_keyboard_event_source()); //REGISTRAMOS EL TECLADO
+
+    placeInvaders( invaders );
+
+    //void al_draw_bitmap(ALLEGRO_BITMAP *bitmap, float dx, float dy, int flags) 
+    al_draw_bitmap(image, 0, 450, 0); //flags(normalmente en cero, ver doc. para rotar etc)
+
+    al_flip_display(); //Flip del backbuffer, pasa a verse a la pantalla
+
+    al_start_timer(timer); //Recien aca EMPIEZA el timer
+
     return 0;
 }
-void placeInvaders(ALLEGRO_BITMAP **ptr_to_invaders, invader_t *ptr_to_struct)
+
+int invaderShot(int i, int j)
 {
-    int enesimoInvader = 0;
-    for (int i = 1; i <= 2; i++)
+    invader_t invader = invaders[i][j];
+    
+    int ship_width = al_get_bitmap_width( invader.invadersPointer );
+    int ship_height = al_get_bitmap_height( invader.invadersPointer );
+    
+    int x_shot = (ship_width + 2* invaders[i][j].x  )/2;
+    int y_shot = invaders[i][j].y + ship_height;
+    
+    shot_t shot = { .x = x_shot,
+                    .y = y_shot,
+                    .type = INVADER_SHOT,
+                    .shotState = 1
+                    };
+
+    int k = 0;
+    while (invaderShotList[k].shotState != 0 && k < MAX_INVADERS_SHOT) {
+        k++;        // Busco un lugar en la lista (donde el disparo no este activo)
+    }
+    if (k < MAX_INVADERS_SHOT) {
+        invaderShotList[k] = shot;
+        actualShots++;
+        al_draw_line(x_shot, y_shot, x_shot, y_shot + 15 , al_color_name("white"), 0);
+        return 0;       // TODO: Cambiar por codigo OK
+    }
+    // ARRAY OVERFLOW
+    return 1;           // TODO: Cambiar por codigo de error
+}
+
+/*******************************************************************************
+ *******************************************************************************
+                        LOCAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+static void placeInvaders(invader_t ptr_to_struct[FIL_INVADERS][COL_INVADERS])
+{
+    for (int i = 1; i <= FIL_INVADERS; i++)
     {
-        for (int j = 1; j <= CANT_INVADERS/2; j++)
+        for (int j = 1; j <= COL_INVADERS; j++)
         {
-            int x_pos =  j * D_WIDTH/(CANT_INVADERS/2 + 1);
-            int y_pos = i * al_get_bitmap_height( ptr_to_invaders[j-1] ) + i*30;
-            al_draw_bitmap( ptr_to_invaders[enesimoInvader], x_pos, y_pos,0 );
-            (ptr_to_struct[enesimoInvader]).x = x_pos;
-            (ptr_to_struct[enesimoInvader]).y = y_pos;
-            (ptr_to_struct[enesimoInvader]).invaderState = 1; //Ademas de colocar las naves, tambien les doy vida en el juego 
-            enesimoInvader++;
+            int x_pos =  j * D_WIDTH/(COL_INVADERS + 1);
+            int y_pos = i * al_get_bitmap_height( ptr_to_struct[i-1][j-1].invadersPointer ) + i*30;
+            al_draw_bitmap( ptr_to_struct[i-1][j-1].invadersPointer, x_pos, y_pos, 0 );
+            ptr_to_struct[i-1][j-1].x = x_pos;
+            ptr_to_struct[i-1][j-1].y = y_pos;
+            ptr_to_struct[i-1][j-1].invaderState = 1; //Ademas de colocar las naves, tambien les doy vida en el juego 
         }
     }
 }
 
-void drawAliveInvaders(ALLEGRO_BITMAP **ptr_to_invaders, invader_t *ptr_to_struct)
+static void drawAliveInvaders(invader_t ptr_to_invaders[FIL_INVADERS][COL_INVADERS])
 {
-    int enesimoInvader = 0;
-
-    for (int i = 0; i < CANT_INVADERS; i++)
+    for (int i = 0; i < FIL_INVADERS; i++)
     {
-        if( (ptr_to_struct[enesimoInvader].invaderState) )
-            al_draw_bitmap( ptr_to_invaders[enesimoInvader], ptr_to_struct[enesimoInvader].x, ptr_to_struct[enesimoInvader].y,0 );  //Dibujo solo los vivos
-        enesimoInvader++;
+        for (int j = 0; j < COL_INVADERS; j++)
+        {
+            if( (ptr_to_invaders[i][j].invaderState) )
+                al_draw_bitmap( ptr_to_invaders[i][j].invadersPointer, ptr_to_invaders[i][j].x, ptr_to_invaders[i][j].y, 0);  //Dibujo solo los vivos        
+        }
     }
+}
 
+/*############################################
+############################################*/
+
+void getInvaderShotCollison(void)
+{
+    if(actualShots > 0)
+    {
+
+        collBoxShot_t canonBox = {  .x = cannonXpos ,
+                                    .y = 450,           // TODO: Hacer una estructura o constante
+                                    .height = al_get_bitmap_height(image),
+                                    .width = al_get_bitmap_width(image)
+                                 };
+
+        int i = 0;
+        int foundShots = 0;
+        int colisionDetected = 0;
+        while (i < MAX_INVADERS_SHOT && foundShots < actualShots) 
+        {
+            if (invaderShotList[i].shotState == 1) {
+                foundShots++;
+
+                al_draw_line( invaderShotList[i].x, invaderShotList[i].y, invaderShotList[i].x , invaderShotList[i].y + 15, al_color_name("white"), 0 );
+                invaderShotList[i].y += TASA_DE_CAMBIO_BALA;
+
+
+                collBoxShot_t collBoxShotFromInvader = {  .x = invaderShotList[i].x - SHOT_WIDTH/2 ,
+                                                          .y = invaderShotList[i].y ,
+                                                          .height = SHOT_HEIGHT , 
+                                                          .width = SHOT_WIDTH
+                                                       };
+
+
+
+                if( isCollision( &collBoxShotFromInvader, &canonBox ) )
+                {
+                    invaderShotList[i].shotState = 0;
+                    colisionDetected++;
+                    printf("COOOOOLLL\n");
+                }
+                else if( invaderShotList[i].y > D_HEIGHT )
+                {
+                    invaderShotList[i].shotState = 0;
+                    actualShots++;
+                }
+            }
+            i++;
+        }
+        actualShots -= colisionDetected;
+
+    }
 }
