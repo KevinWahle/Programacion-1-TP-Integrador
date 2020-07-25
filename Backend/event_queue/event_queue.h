5734b454f8af3a06dc569d7524a33aadd3d79690
sergioandyp <sergioandyp@gmail.com>
2020-07-25 11:55:14 -0300

    event_queue funcionando y con objeto

--- Backend/event_queue/event_queue.h
+++ Backend/event_queue/event_queue.h
@@ -1,7 +1,7 @@
 /***************************************************************************//**
-  @file     +Nombre del archivo (ej: template.h)+
-  @brief    +Descripcion del archivo+
-  @author   +Nombre del autor (ej: Salvador Allende)+
+  @file     +event_queue.h+
+  @brief    +Archivo header del módulo event_queue, para crear una cola de eventos+
+  @author   +Grupo 3+
  ******************************************************************************/
 
 #ifndef _EVENT_QUEUE_H_
@@ -17,9 +17,9 @@
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
 
-#define MAX_EVENTS  500
+#define MAX_EVENTS  500                 // Máxima cantidad de eventos
 
-#define NULL_EVENT  ((event_t)(-1))
+#define NULL_EVENT  ((event_t)(-1))     // Evento nulo (máximo valor de event_t)
 
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
@@ -31,8 +31,6 @@ typedef uint16_t event_t;
  * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
 
-// +ej: extern unsigned int anio_actual;+
-
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
@@ -40,7 +38,7 @@ typedef uint16_t event_t;
 
 
 /**
- * @brief Agrega un evento a la lista
+ * @brief Agrega un evento a la cola
  * @param event evento a añadir a la cola
  * @return 1 si se completo, 0 si la cola está llena
 */
@@ -48,19 +46,27 @@ int add_event(event_t event);
 
 /**
  * @brief Obtiene el próximo evento en la cola
- * @return El último evento de la cola, NULL_EVENT si la cola está vacía
+ * @return El próximo evento en la cola, NULL_EVENT si la cola está vacía
 */
 event_t get_next_event(void);
 
 /**
- * @brief Elimina el último elemento agregado a la cola
- * @return 1 si se completo, 0 si la cola está llena
+ * @brief Omite el próximo elemento en la cola
+ * @return 0 Si hubo éxito, 1 si la cola estaba vacía
 */
-int remove_last_event(void);
+int skip_event(void);
 
-//int remove_eventa_at_index(size_t index);
-
+/**
+ * @brief Vacía la cola
+*/
 void empty_queue(void);
+
+/**
+ * @brief Indica si la cola está vacía o no
+ * @return 1 si la cola estaba vacía, caso contrario, 0
+*/
+int is_queue_empty(void);
+
 /*******************************************************************************
  ******************************************************************************/
 
