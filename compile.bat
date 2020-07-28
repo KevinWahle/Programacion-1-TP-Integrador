f881b5fd82b19df3f3fab956361d0ca0ac24c563
sergioandyp <sergioandyp@gmail.com>
2020-07-28 20:31:27 -0300

    added -l allegro_ttf

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 echo Compilando...
 @echo off
 REM Uso: compile "source" "target". Ej: compile main.c main
-gcc %1 -Wall -o %2 -l allegro -l allegro_audio -l allegro_acodec -l allegro_color -l allegro_font -l allegro_image -l allegro_primitives
+gcc %1 -Wall -o %2 -l allegro -l allegro_audio -l allegro_acodec -l allegro_color -l allegro_font -l allegro_image -l allegro_primitives -l allegro_ttf
