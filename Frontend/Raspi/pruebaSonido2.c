fd7dfbd49e7d92d028f2bf4acb2e7cb88dd9905e
bautistasch <bschneeberger@itba.edu.ar>
2021-01-18 15:30:43 -0300

    ...

--- Frontend/Raspi/pruebaSonido2.c
+++ Frontend/Raspi/pruebaSonido2.c
@@ -27,7 +27,7 @@ int main(void)
         printf("OSTRAS, SE DEBERIA DISPARAR YA\n");
 
             //playSoundFromMemory(sound, SDL_MIX_MAXVOLUME);
-            playSound("../Sounds/shoot.wav", 128/2);
+            playMusic("../Sounds/shoot.wav", 128/2);
         
     }
 }
